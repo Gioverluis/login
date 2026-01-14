8424ce28be40d58b8b742274704b209b02dc9ca3
Giover luis <gioverl.matos28@gmail.com>
2026-01-14 03:18:49 -0500

    codigo mejorado

---
+++ js/app.js
@@ -0,0 +1,7 @@
+document.getElementById('bt-registro').addEventListener ('click', () => {
+    setTimeout(() => window.location.href = 'registro.html', 600);
+})
+/*
+document.getElementById('init-section').addEventListener ('click', () => {
+   setTimeout(() => window.location.href = 'index.html', 600);
+})  */
